80215741c189f91f03a03f8b3f179dbddafa5d12
Raíssa Dias <raissablw@gmail.com>
2026-01-28 17:13:06 -0300

    feat(back-end): adicionar cache nas buscas ao google books

--- app/Services/GoogleBooksService.php
+++ app/Services/GoogleBooksService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 use function Illuminate\Log\log;
@@ -20,34 +21,37 @@ class GoogleBooksService
      */
     public function search(string $query): array
     {
-        $response = Http::get('https://www.googleapis.com/books/v1/volumes', [
-            'q' => $query,
-            'maxResults' => 10,
-            'key' => $this->apiKey
-        ]);
+        # Cache results for 1 hour
+        return Cache::remember("search_{$query}", 3600, function () use ($query) {
+            $response = Http::get('https://www.googleapis.com/books/v1/volumes', [
+                'q' => $query,
+                'maxResults' => 10,
+                'key' => $this->apiKey
+            ]);
 
-        $data = $response->json();
+            $data = $response->json();
 
-        if (empty($data['items'])) {
-            return [];
-        }
+            if (empty($data['items'])) {
+                return [];
+            }
 
-        # Format the results
-        return collect($data['items'])->map(function ($item) {
-            $info = $item['volumeInfo'];
-            $isbn = $info['industryIdentifiers'][0]['identifier'] ?? null;
+            # Format the results
+            return collect($data['items'])->map(function ($item) {
+                $info = $item['volumeInfo'];
+                $isbn = $info['industryIdentifiers'][0]['identifier'] ?? null;
 
-            return [
-                'id' => $item['id'],
-                'title' => $info['title'] ?? 'Sem Título',
-                'author' => $info['authors'][0] ?? 'Desconhecido',
-                'isbn' => $isbn,
-                'page_count' => $info['pageCount'] ?? null,
-                'image_url' => isset($info['imageLinks']['thumbnail']) 
-                    ? str_replace('http://', 'https://', $info['imageLinks']['thumbnail']) 
-                    : null,
-            ];
-        })->toArray();
+                return [
+                    'id' => $item['id'],
+                    'title' => $info['title'] ?? 'Sem Título',
+                    'author' => $info['authors'][0] ?? 'Desconhecido',
+                    'isbn' => $isbn,
+                    'page_count' => $info['pageCount'] ?? null,
+                    'image_url' => isset($info['imageLinks']['thumbnail']) 
+                        ? str_replace('http://', 'https://', $info['imageLinks']['thumbnail']) 
+                        : null,
+                ];
+            })->toArray();
+        });
     }
 
     /**
@@ -55,39 +59,42 @@ class GoogleBooksService
      */
     public function getBookDetails(string $googleBookId): ?array
     {
-        $response = Http::get("https://www.googleapis.com/books/v1/volumes/{$googleBookId}", [
-            'key' => $this->apiKey
-        ]);
+        # Cache results for 7 days
+        return Cache::remember("book_details_{$googleBookId}", 60 * 60 * 24 * 7, function () use ($googleBookId) {
+            $response = Http::get("https://www.googleapis.com/books/v1/volumes/{$googleBookId}", [
+                'key' => $this->apiKey
+            ]);
 
-        if ($response->failed()) {
-            return null;
-        }
+            if ($response->failed()) {
+                return null;
+            }
 
-        $data = $response->json();
+            $data = $response->json();
 
-        if (empty($data['volumeInfo'])) {
-            return null;
-        }
+            if (empty($data['volumeInfo'])) {
+                return null;
+            }
 
-        $info = $data['volumeInfo'];
-        $isbn = $info['industryIdentifiers'][0]['identifier'] ?? null;
+            $info = $data['volumeInfo'];
+            $isbn = $info['industryIdentifiers'][0]['identifier'] ?? null;
 
-        # Format the results
-        return [
-            'id' => $data['id'],
-            'title' => $info['title'] ?? 'Sem Título',
-            'authors' => $info['authors'] ?? ['Desconhecido'],
-            'description' => $info['description'] ?? '',
-            'publisher' => $info['publisher'] ?? '',
-            'published_date' => $info['publishedDate'] ?? '',
-            'page_count' => $info['pageCount'] ?? null,
-            'categories' => $info['categories'] ?? [],
-            'average_rating' => $info['averageRating'] ?? null,
-            'ratings_count' => $info['ratingsCount'] ?? null,
-            'isbn' => $isbn,
-            'image_url' => isset($info['imageLinks']['thumbnail']) 
-                ? str_replace('http://', 'https://', $info['imageLinks']['thumbnail']) 
-                : null,
-        ];
+            # Format the results
+            return [
+                'id' => $data['id'],
+                'title' => $info['title'] ?? 'Sem Título',
+                'authors' => $info['authors'] ?? ['Desconhecido'],
+                'description' => $info['description'] ?? '',
+                'publisher' => $info['publisher'] ?? '',
+                'published_date' => $info['publishedDate'] ?? '',
+                'page_count' => $info['pageCount'] ?? null,
+                'categories' => $info['categories'] ?? [],
+                'average_rating' => $info['averageRating'] ?? null,
+                'ratings_count' => $info['ratingsCount'] ?? null,
+                'isbn' => $isbn,
+                'image_url' => isset($info['imageLinks']['thumbnail']) 
+                    ? str_replace('http://', 'https://', $info['imageLinks']['thumbnail']) 
+                    : null,
+            ];
+        });
     }
 }
